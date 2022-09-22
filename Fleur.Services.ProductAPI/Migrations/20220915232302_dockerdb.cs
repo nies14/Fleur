66b27dfc822e1684c0ddb1d5e7b7413f7f27f879
Tanvir Hassan <mthnies@gmail.com>
2022-09-21 21:53:31 -0400

    added docker and docker compose for product api service

---
+++ Fleur.Services.ProductAPI/Migrations/20220915232302_dockerdb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fleur.Services.ProductAPI.Migrations
+{
+    public partial class dockerdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Fleur.Services.ProductAPI/Migrations/20220918003801_initialMigrationToSqlDockerImage.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fleur.Services.ProductAPI.Migrations
+{
+    public partial class initialMigrationToSqlDockerImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
